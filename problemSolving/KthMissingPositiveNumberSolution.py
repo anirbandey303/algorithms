4ab9b7aa8801fa5d8bf273a6b7f27a08a9d74337
Anirban Dey <anirbandey303@gmail.com>
2021-01-07 02:12:39 +0530

    Kth Missing Positive Number

---
+++ problemSolving/KthMissingPositiveNumberSolution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        count = 0
+        i = 0
+        while(count != k):
+            i += 1
+            if(i not in arr):
+                count += 1
+        return i
