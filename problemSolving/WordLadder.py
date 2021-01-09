8dc702a6c5ae7f884a1ff5c532e87044b0740e18
Anirban Dey <anirbandey303@gmail.com>
2021-01-10 02:39:38 +0530

    refactored Word Ladder

--- problemSolving/WordLadder.py
+++ problemSolving/WordLadder.py
@@ -35,3 +35,40 @@ class Solution:
                             #print(queue)
                 lCount -= 1
         return 0
+
+
+#################################################################################################
+#v0.2
+
+import string
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        mySet = set()
+        for word in wordList:
+            mySet.add(word)
+        if(endWord not in mySet):
+            return 0
+        #alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        queue = list()
+        depth = 0
+        queue.append(beginWord)
+        
+        while(queue):
+            depth += 1
+            levelCount = len(queue)
+            while(levelCount > 0):
+                word = queue.pop(0)
+                for i in range(len(word)):
+                    for j in string.ascii_lowercase:
+                        temp = list(word)
+                        temp[i] = j
+                        temp = ''.join(temp)
+                        if(temp == word):
+                            continue
+                        if(temp == endWord):
+                            return depth + 1
+                        if(temp in mySet):
+                            queue.append(temp)
+                            mySet.remove(temp)
+                levelCount -= 1
+        return 0
