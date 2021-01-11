0bf2c023485d1e0da6ebbcb698e79fa199e97b50
Anirban Dey <anirbandey303@gmail.com>
2021-01-11 21:28:28 +0530

    Merge sort Reversed --Better Approach

--- problemSolving/MergeSortedArray.py
+++ problemSolving/MergeSortedArray.py
@@ -30,3 +30,23 @@ class Solution:
             n2+=1
         for i in range(len(nums1) - totalSize):
             nums1.pop()
+
+
+#########BEtter approach##############
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        m-=1
+        n-=1
+        for i in reversed(range(len(nums1))):
+            if(n<0):
+                return
+            if(m>=0 and nums1[m] > nums2[n]):
+                nums1[i] = nums1[m]
+                m -= 1
+            else:
+                nums1[i] = nums2[n]
+                n -=1
