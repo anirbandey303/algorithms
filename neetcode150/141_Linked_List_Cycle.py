3ae8b72fc4916cba28d59378881ddc5149c51e90
Anirban Dey <anirbandey303@gmail.com>
2025-08-20 01:21:46 -0400

    2nd approach to detect linked list cycle

--- neetcode150/141_Linked_List_Cycle.py
+++ neetcode150/141_Linked_List_Cycle.py
@@ -58,3 +58,14 @@ class Solution:
             head = head.next
         return False
     
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+
+        while(fast and fast.next):
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
+    
